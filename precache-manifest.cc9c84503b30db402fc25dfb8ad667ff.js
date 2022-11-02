38eaed038320a79dc758f777751a8b6b7fb3eec9
YayanHD <yayanhid@gmail.com>
2022-11-02 08:24:04 +0700

    Updates

---
+++ precache-manifest.cc9c84503b30db402fc25dfb8ad667ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edfaffa5244bba7a15ad",
+    "url": "/notes-app/static/js/main.edfaffa5.chunk.js"
+  },
+  {
+    "revision": "664b7d871ff8ad87d3cd",
+    "url": "/notes-app/static/js/1.664b7d87.chunk.js"
+  },
+  {
+    "revision": "3e66a518e6a1e51b852c",
+    "url": "/notes-app/static/js/runtime~main.3e66a518.js"
+  },
+  {
+    "revision": "7f34a9f15329bcf6f434bf58788a8512",
+    "url": "/notes-app/static/media/archive-btn.7f34a9f1.png"
+  },
+  {
+    "revision": "1950df1f99e0ea26bce74afa4b4bb53e",
+    "url": "/notes-app/static/media/unarchive-btn.1950df1f.png"
+  },
+  {
+    "revision": "d30319e9d672f95aff8d105e50390d1b",
+    "url": "/notes-app/index.html"
+  }
+];
